e740777babd9208cfb1218e519b37d37252c1d00
Prem Ranjan <ranjanprem281204@gmail.com>
2026-03-19 21:32:29 +0530

    Initial commit (frontend +backend)

---
+++ backend/quiz/src/main/java/com/prem/quiz/repo/QuestionRepo.java
@@ -0,0 +1,9 @@
+package com.prem.quiz.repo;
+
+import com.prem.quiz.entity.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<QuizQuestion,Long> {
+}
